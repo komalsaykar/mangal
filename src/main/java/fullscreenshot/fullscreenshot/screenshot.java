1bae46a87410eef69ea10d84da733dc09d7cd8a9
komalsaykar <134285462+komalsaykar@users.noreply.github.com>
2023-09-28 01:01:44 -0700

    Update screenshot.java

--- src/main/java/fullscreenshot/fullscreenshot/screenshot.java
+++ src/main/java/fullscreenshot/fullscreenshot/screenshot.java
@@ -19,8 +19,6 @@ public class screenshot {
 		WebDriver driver =  new EdgeDriver();
 		driver.get("https://www.amazon.in/");
 		driver.manage().window().maximize();
-		Screenshot scr = new AShot().shootingStrategy(ShootingStrategies.viewportPasting(100)).takeScreenshot(driver);
-		ImageIO.write(scr.getImage(), "PNG", new File("C:\\Users\\HP\\eclipse-workspace\\TestNG_endtoend\\FullScreenshot\\a1.png"));
 		
 
 	}
